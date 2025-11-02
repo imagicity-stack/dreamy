4d35e72ba89d50f904db1a3798e381c3eebcbee9
imagicity-stack <imagicityart@gmail.com>
2025-11-02 22:44:42 +0530

    Remove animated section divider line

--- src/components/SectionDivider.tsx
+++ src/components/SectionDivider.tsx
@@ -1,9 +1,5 @@
 import { memo } from "react";
 
-const SectionDivider = () => (
-  <div className="w-full flex justify-center py-8" aria-hidden="true">
-    <div className="section-divider-line" />
-  </div>
-);
+const SectionDivider = () => <div className="w-full py-8" aria-hidden="true" />;
 
 export default memo(SectionDivider);
